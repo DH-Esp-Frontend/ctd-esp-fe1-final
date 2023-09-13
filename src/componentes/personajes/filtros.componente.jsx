55b93794492fa5bc2b0535c0c8b11def39e134bd
Estebanvasquex <53843278+Estebanvasquex@users.noreply.github.com>
2023-09-13 18:54:30 -0500

    Update filtros.componente.jsx

--- src/componentes/personajes/filtros.componente.jsx
+++ src/componentes/personajes/filtros.componente.jsx
@@ -3,7 +3,7 @@ import './filtros.css';
 const Filtros = () => {
 
     return <div className="filtros">
-        <label for="nombre">Filtrar por nombre:</label>
+        <label htmlFor="nombre">Filtrar por nombre:</label>
         <input type="text" placeholder="Rick, Morty, Beth, Alien, ...etc" name="nombre" />
     </div>
 }
