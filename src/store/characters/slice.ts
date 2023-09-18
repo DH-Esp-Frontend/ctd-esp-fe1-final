1b2c55101bca71922aa42e1c444005736ed0ea18
Carmen Vargas <trishiavargas@gmail.com>
2023-09-18 20:04:41 -0300

    "modificando slice y thunk"

--- src/store/characters/slice.ts
+++ src/store/characters/slice.ts
@@ -1,1 +1,87 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { GET_CHARACTERS, GET_CHARACTERS_FILTER, } from './thunk';
+import { IAllCharacters, ICharacter} from '../../interfaces/character.interface';
+
+
+
+export type CharacterState = {
+urlBase: string
+allCharacters: ICharacter[],
+isLoading: boolean,
+isError: string | null,
+nextPage: string,
+prevPage: string,
+esfavorito: boolean
+listFavoritos: ICharacter[]
+
+}
+
+
+const initialState : CharacterState= {
+    urlBase: 'https://rickandmortyapi.com/api/character/',
+    allCharacters: [],
+    isLoading: true,
+    isError :  null,
+    prevPage: '',
+    nextPage: '',
+    esfavorito: false,
+    listFavoritos: []
+}
+
+
+export const charactersSlice = createSlice({
+    name : 'character',
+    initialState: initialState,
+    reducers : {
+        ADD_FAVORITOS : (state, action : PayloadAction<number>) => {
+            const findCharacter = state.allCharacters.find(character => character.id === action.payload);
+            if(findCharacter){
+                findCharacter.esFavorito = !state.esfavorito;
+                state.listFavoritos.push(findCharacter)
+            }
+        },
+      
+    },
+    extraReducers : (builder) =>{
+        builder.addCase(GET_CHARACTERS.pending, ( state   ) => {
+            state.isLoading = true;
+        })
+
+        builder.addCase(GET_CHARACTERS.fulfilled, (state, action : PayloadAction<IAllCharacters>) => {
+            state.allCharacters = action.payload.allCharacters;
+            state.prevPage = action.payload.prevPage
+            state.nextPage = action.payload.nextPage
+            state.isLoading = false;
+        })
+
+        builder.addCase(GET_CHARACTERS.rejected, ( state, action ) => {
+            state.isLoading = false;
+            state.isError = action.error.message ?? 'Ha ocurrido un error'
+            
+            
+        })
+        builder.addCase(GET_CHARACTERS_FILTER.pending, ( state   ) => {
+            state.isLoading = true;
+        })
+
+        builder.addCase(GET_CHARACTERS_FILTER.fulfilled, (state, action : PayloadAction<IAllCharacters>) => {
+            state.allCharacters = action.payload.allCharacters;
+            state.prevPage = action.payload.prevPage
+            state.nextPage = action.payload.nextPage
+            state.isLoading = false;
+        })
+
+        builder.addCase(GET_CHARACTERS_FILTER.rejected, ( state, action ) => {
+            state.isLoading = false;
+            state.isError = action.error.message ?? 'Ha ocurrido un error'
+            
+            
+        })
+},
+
+});
+
+
+const characterReducer = charactersSlice.reducer;
+export const {ADD_FAVORITOS} = charactersSlice.actions;
+export default characterReducer;
--- src/store/characters/thunk.ts
+++ src/store/characters/thunk.ts
@@ -1,26 +1,28 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { IAllCharacters,ICharacter } from "../../interfaces/character.interface";
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import { IAllCharacters, ICharacter } from '../../interfaces/character.interface'
 
 export const GET_CHARACTERS = createAsyncThunk(
-    'character/GET_CHARACTERS',
-    async (urlBase: string): Promise<IAllCharacters> => {
-            
-            try {
-                    const resp = await fetch(urlBase);
-                    const data = await resp.json();
-                    const resultsCharacters = {
-                            allCharacters: data.results,
-                            nextPage: data.info.next,
-                            prevPage: data.info.prev
-                    }
-                    return resultsCharacters;
-                  } catch (error) {
-                    console.error('Error fetching data:', error);
-                    throw error;
-                  }
-                 
-    });
-    export const GET_CHARACTERS_FILTER = createAsyncThunk(
+        'character/GET_CHARACTERS',
+        async (urlBase: string): Promise<IAllCharacters> => {
+                
+                try {
+                        const resp = await fetch(urlBase);
+                        const data = await resp.json();
+                        const resultsCharacters = {
+                                allCharacters: data.results,
+                                nextPage: data.info.next,
+                                prevPage: data.info.prev
+                        }
+                        return resultsCharacters;
+                      } catch (error) {
+                        console.error('Error fetching data:', error);
+                        throw error;
+                      }
+                     
+        }
+      );
+   
+      export const GET_CHARACTERS_FILTER = createAsyncThunk(
         'character/GET_CHARACTERS_FILTER',
         async ({name}: {name: string | null | undefined}): Promise<IAllCharacters> => {
                 
@@ -40,7 +42,9 @@ export const GET_CHARACTERS = createAsyncThunk(
                      
         }
       );
-      export const GET_CHARACTER_ID = createAsyncThunk('character/GET_CHARACTERS_ID', async (id: number) : Promise<ICharacter>=> {
+   
+
+export const GET_CHARACTER_ID = createAsyncThunk('character/GET_CHARACTERS_ID', async (id: number) : Promise<ICharacter>=> {
         const resp = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
         const data = await resp.json();
         return data;
